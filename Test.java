8351bd3d854d3a7ac691a1f7508da4ea1e979a03
OPERATIVE\brkumar <brkumar@operative.com>
2021-08-09 20:40:18 +0530

    fibonassi series

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        int n=10;
+        int[] arr=new int[n+2];
+        arr[0]=0;
+        arr[1]=1;
+        for (int i = 2; i < n; i++) {
+            arr[i]=arr[i-1]+arr[i-2];
+            System.out.print(arr[i]);
+            System.out.print(",");
+        }
+
+
+    }
+}
