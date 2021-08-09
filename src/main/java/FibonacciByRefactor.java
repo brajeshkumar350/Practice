0aa6c1838032c66ddbc5d120175c769dc40502dc
OPERATIVE\brkumar <brkumar@operative.com>
2021-08-09 20:52:56 +0530

    add fibonacci series vaious ways

---
+++ src/main/java/FibonacciByRefactor.java
@@ -0,0 +1,17 @@
+public class FibonacciByRefactor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    static int fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        // Recursive call
+        return fibonacci(n - 1)
+                + fibonacci(n - 2);
+    }
+}
---
+++ src/main/java/FibonacciByVar.java
@@ -0,0 +1,15 @@
+public class FibonacciByVar {
+    public static void main(String[] args) {
+        int num1=0;
+        int num2=1;
+        int num;
+        System.out.print(num1+","+num2);
+        for (int i = 0; i < 10; i++) {
+            num=num1+num2;
+            System.out.print(",");
+            System.out.print(num);
+            num1=num2;
+            num2=num;
+        }
+    }
+}
